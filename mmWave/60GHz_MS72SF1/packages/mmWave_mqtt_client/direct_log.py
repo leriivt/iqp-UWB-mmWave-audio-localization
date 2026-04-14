b718ce9e1a74961f646165563bb69cf6569b8231
Verit Li <verit.li@gmail.com>
2026-04-14 10:32:31 +0800

    logging python that can be run for debugging

---
+++ mmWave/60GHz_MS72SF1/packages/mmWave_mqtt_client/direct_log.py
@@ -0,0 +1,50 @@
+from .mmWave_hardware import *
+import time
+
+config = mmWaveConfig()
+hardware_interface = mmWaveHardwareInterface(config, mode=0)
+
+def log_mmWave_frame_personnel(frame: mmWaveFrame):
+    logger.info(json.dumps({
+        "event": "new_mmWave_frame",
+        "frame_count": frame.current_frame_count,
+        "num_personnel": len(frame.personnel),
+        "personnel": [
+            {
+                "id": p.id,
+                "x": p.x,
+                "y": p.y,
+                "z": p.z,
+                "speed_x": p.speed_x,
+                "speed_y": p.speed_y,
+                "speed_z": p.speed_z
+            }
+            for p in frame.personnel
+        ]
+    }))
+
+def log_mmWave_frame_point_cloud(frame: mmWaveFrame):
+    logger.info(json.dumps({
+        "event": "new_mmWave_frame",
+        "frame_count": frame.current_frame_count,
+        "num_points": len(frame.point_cloud),
+        "point_cloud": [
+            {
+                "x": p.x,
+                "y": p.y,
+                "z": p.z,
+            }
+            for p in frame.point_cloud
+        ]
+    }))
+
+hardware_interface.set_measurement_callback(log_mmWave_frame_personnel)
+hardware_interface.start()
+
+#loop forever, can be stopped with Ctrl+C
+#otherwise the daemon thread will just kill itself when this main script exits
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    hardware_interface.stop()
