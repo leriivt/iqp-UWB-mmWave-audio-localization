eb42f237dc873161ea8fd15492478091fe5ab5e8
Verit Li <verit.li@gmail.com>
2026-04-15 11:06:39 +0800

    ai gen PyQt point cloud visualizer

---
+++ mmWave/60GHz_MS72SF1/packages/mmWave_mqtt_client/direct_point_cloud.py
@@ -0,0 +1,17 @@
+from mmWave_hardware import *
+import time
+
+config = mmWaveConfig()
+hardware_interface = mmWaveHardwareInterface(config, mode=1)
+
+
+from mmwave_visualizer import *
+
+
+app = QApplication(sys.argv)
+
+window = mmWaveVisualizer()
+integrate_with_hardware(window, hardware_interface)   # hardware_interface is your mmWaveHardwareInterface
+hardware_interface.start()
+window.show()
+sys.exit(app.exec_())
